bd99059346492b65c19186abc3cb9945a6388143
Darianopolis <darianopolis@gmail.com>
2023-05-25 22:18:33 +0100

    Removed Overlay and box test

--- modules/launch/bldr.lua
+++ modules/launch/bldr.lua
@@ -2,15 +2,11 @@ if Project "nms-v2" then
     Compile {
         "src/common/**",
         "src/indexer/**",
-        "src/overlay/*", -- Don't compile examples
-        "src/overlay/directx/*",
         "src/nomoreshortcuts/**",
-        -- "src/overlay/example/BoxDemo.cpp"
     }
     Include {
         "src/common",
         "src/indexer",
-        "src/overlay",
         "src/nomoreshortcuts",
         "src/nomoreshortcuts/core",
     }
@@ -21,23 +17,4 @@ if Project "nms-v2" then
 
     Artifact { "out/NoMoreShortcuts", type = "Window" }
     -- Artifact { "out/main", type = "Console" }
-end
-
-if Project "nms-v2-box" then
-    Compile {
-        "src/boxtest/**",
-
-        "src/nomoreshortcuts/core/Platform.cpp",
-    }
-    Include {
-        "src/common",
-        "src/indexer",
-        "src/overlay",
-        "src/nomoreshortcuts",
-        "src/nomoreshortcuts/core",
-    }
-    Import {
-        "nova",
-    }
-    Artifact { "out/main", type = "Console" }
 end
